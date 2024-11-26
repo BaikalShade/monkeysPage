6bb9d929de42220461dfcadb7b8e614703e24596
Batorov03 <itishn1k.19@gmail.com>
2024-11-26 13:24:51 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <test-task></test-task>
+  </div>
+</template>
+
+<script setup>
+import TestTask from './components/TestTask.vue';
+</script>
+
+<style>
+#app {
+  max-width: 800px;
+  margin: 0px auto;
+  padding: 20px;
+}
+body {
+  margin: 0;
+  font-family: roboto;
+  background-color: #282c34;
+  color: white;
+}
+</style>
